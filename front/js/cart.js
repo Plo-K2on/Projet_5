9bc30ed5ddb9be7476bf6a78fa4845bc4084e0a1
Hugo CF <hcaffinpgmail.com>
2022-04-29 11:42:00 +0200

    Début de cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,13 +1,12 @@
 
+// Relier l'onglet product.js à l'onglet cart.js
+// Récuperer allItems depuis le localstorage
+// Ajouter la possibilitée de modifier un article dans le panier
+// Ajouter la possibilitée de supprimer un article dans le panier
+// Récupérer et analyser les données saisies par l’utilisateur dans le formulaire.
+// Afficher un message d’erreur si besoin.
+// Créer un objet contact
+// Créer un tableau de produits.
+// Effectuer une requête POST sur l’API et récupérer l’identifiant de commande dans la réponse de celle-ci.
+// Rediriger l’utilisateur sur la page Confirmation, en passant l’id decommande dans l’URL, dans le but d’afficher le numéro de commande
 
-// let allItems = []
-
-// let itemCart = 0;
-// let totalItems = itemCart;
-
-// class items {
-//     constructor(colors, quantity) {
-//       this.colors = color;
-//       this.quantity = quantity;
-//     }
-//   }
