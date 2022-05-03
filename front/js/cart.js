c2928932ed3b49071780e77dabfa3e003875afd0
Hugo CF <hcaffinpgmail.com>
2022-05-03 12:45:04 +0200

    03/05/22

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,5 @@
 
-// Récuperer allItems depuis le localstorage
+// Récuperer les elements du panier depuis le localstorage
 // Faire afficher les éléments de allItems (le panier) dans la page panier
 // Ajouter la possibilitée de modifier un article dans le panier
 // Ajouter la possibilitée de supprimer un article dans le panier
@@ -13,18 +13,44 @@
 // Rediriger l’utilisateur sur la page Confirmation, en passant l’id de commande dans l’URL, dans le but d’afficher le numéro de commande sur la page de confirmation
 
 
-function main() {
-    console.log("ma fonction principale démarre ici")
+function principal() {
+  console.log("ma fonction principale démarre ici")
+  // Récuperer les elements du panier depuis le localstorage
+  let allItems = JSON.parse(localStorage.getItem('panier')) || []
+
+  // Faire afficher les éléments de allItems (le panier) dans la page panier
+  // j'appelle ma fonction affichePanier
+  affichePanier(allItems)
+}
   
-    // j'appelle ma fonction displayCart
-    displayCart()
-  }
+// fonction pour afficher le contenu du localstorage sur la page panier
+function affichePanier(elementsPanier) {
+  console.log("je dois afficher le panier, comment faire Hugo ? ")
+  elementsPanier.forEach(element => {
+    console.log("element", element)
+    // soit utiliser la meme methode que dans la page scripts
+
+    // ou utiliser
+
+    const cartContainer = document.getElementById("cart__items")
+    cartContainer.innerHTML += 
+    `
+    <h3>${element.info.name}</h3>
+    <p>${element.info.altTxt}</p>
+    <p>${element.info.colors}</p>
+    <p>${element.info.description}</p>
+    <p>${element.info.price}</p>
+    <p>${element.info._id}</p>
+    <div class="cart__item__img">
+      <img src="${element.info.imageUrl}" alt="Photographie d'un canapé">
+    </div>
+    `
+
+  });
+}
   
-  // fonction pour afficher le contenu du localstorage sur la page panier
-  function displayCart() {
-    console.log("je dois afficher le panier, comment faire Hugo ? ")
-  }
-  
-  // Ma page se charge j'appelle ma fonction principale qui est exécuté en premier
-  main();
+// Ma page se charge j'appelle ma fonction principale qui est exécuté en premier
+principal();
+
+// gestionnaires des evènements
   
